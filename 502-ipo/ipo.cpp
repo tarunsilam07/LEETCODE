73cb92324b9ce069af17f89abf677bcb23739c6f
tarunsilam07 <tarun79767@gmail.com>
2024-06-15 21:59:14 +0530

    Time: 130 ms (87.46%) | Memory: 85.5 MB (63.02%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
-       int n=profits.size();
-       std::vector<std::pair<int,int>>pro;
-       for(int i=0;i<n;i++)
-       {
-        pro.emplace_back(capital[i],profits[i]);
-       } 
-       sort(pro.begin(),pro.end());
-       priority_queue<int>maxHeap;
-       int i=0;
-       for(int j=0;j<k;j++)
-       {
-        while(i<n && pro[i].first<=w)
+        int n=profits.size();
+        vector<pair<int,int>>projects;
+        for(int i=0;i<n;i++)
         {
-            maxHeap.push(pro[i].second);
-            i++;
+            projects.emplace_back(capital[i],profits[i]);
         }
-        if(maxHeap.empty())
+        sort(projects.begin(),projects.end());
+        int i=0;
+        priority_queue<int>maxHeap;
+        for(int j=0;j<k;j++)
         {
+            while(i<n && projects[i].first<=w)
+            {
+                maxHeap.push(projects[i].second);
+                i++;
+            }
+            if(maxHeap.empty())
             break;
+            w+=maxHeap.top();
+            maxHeap.pop();
         }
-        w+=maxHeap.top();
-        maxHeap.pop();
-       }
-       return w;
+        return w;
     }
 };
