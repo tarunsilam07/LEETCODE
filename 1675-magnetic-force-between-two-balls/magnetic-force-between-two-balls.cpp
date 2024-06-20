1bc6b4edc4d3a2c5309aa54ac2d76d4074440e2d
tarunsilam07 <tarun79767@gmail.com>
2024-06-20 10:47:59 +0530

    Time: 129 ms (43.95%) | Memory: 61.3 MB (73.97%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -1,38 +1,37 @@
 class Solution {
 public:
-    int maxDistance(vector<int>& nums, int m) {
-     sort(nums.begin(),nums.end());
-     int low=1;
-     int n=nums.size();
-     int high=nums[n-1]; 
-     int ans=-1;
-     while(low<=high)
-     {
-        int mid=low+(high-low)/2;
-        if(canweplace(nums,mid,m))
+    int maxDistance(vector<int>& arr, int m) {
+        sort(arr.begin(),arr.end());
+        int low=1;
+        int n=arr.size();
+        int high=arr[n-1];
+        int ans=-1;
+        while(low<=high)
         {
-            ans=mid;
-            low=mid+1;
+            int mid=low+(high-low)/2;
+            if(canweplace(arr,mid,m))
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else high=mid-1;
         }
-        else high=mid-1;
-     }  
-     return ans;
+        return ans;
     }
-    int canweplace(vector<int>&nums,int distance,int k)
+    int canweplace(vector<int>&arr,int distance,int k)
     {
         int mags=1;
-        int last=nums[0];
-        for(int i=1;i<nums.size();i++)
+        int last=arr[0];
+        for(int i=1;i<arr.size();i++)
         {
-            if(nums[i]-last>=distance)
+            if(arr[i]-last>=distance)
             {
                 mags++;
-                last=nums[i];
+                last=arr[i];
             }
         }
         if(mags>=k)
-        return true;
+            return true;
         return false;
     }
-    
 };
