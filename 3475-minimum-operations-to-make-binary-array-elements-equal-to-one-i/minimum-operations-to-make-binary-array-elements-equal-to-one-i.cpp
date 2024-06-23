705071c62528a33496b9628693e7796f981dba71
tarunsilam07 <tarun79767@gmail.com>
2024-06-23 21:36:53 +0530

    Time: 116 ms (60.00%) | Memory: 138.4 MB (60.00%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -5,19 +5,20 @@ public:
         int cnt=0;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]==0 && i+2<n)
+            if(nums[i]==0)
             {
+                if(i+2>=n) return -1;
                 nums[i]=1;
                 (nums[i+1]==0) ?nums[i+1]=1 :nums[i+1]=0;
                 (nums[i+2]==0) ?nums[i+2]=1 :nums[i+2]=0;
                 cnt++;
             }
         } 
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==0)
-            return -1;
-        } 
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //     return -1;
+        // } 
         return cnt;
     }
 };
