a59272b07ebcd135bb15e9a571637c67f9306ae4
tarunsilam07 <tarun79767@gmail.com>
2024-07-03 15:15:46 +0530

    Time: 74 ms (28.72%) | Memory: 38.7 MB (50.84%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -4,11 +4,11 @@ public:
         sort(nums.begin(),nums.end());
         if(nums.size()<=3) return 0;
         int n=nums.size()-1;
-        vector<int>ans;
-        ans.push_back(nums[n]-nums[3]);
-        ans.push_back(nums[n-3]-nums[0]);
-        ans.push_back(nums[n-1]-nums[2]);
-        ans.push_back(nums[n-2]-nums[1]);
-        return *min_element(ans.begin(),ans.end());
+        int mini=INT_MAX;
+        if((nums[n]-nums[3])<mini) mini=(nums[n]-nums[3]);
+        if((nums[n-3]-nums[0])<mini) mini=nums[n-3]-nums[0];
+        if((nums[n-1]-nums[2])<mini) mini=(nums[n-1]-nums[2]);
+        if((nums[n-2]-nums[1])<mini) mini=(nums[n-2]-nums[1]);
+        return mini;
     }
 };
