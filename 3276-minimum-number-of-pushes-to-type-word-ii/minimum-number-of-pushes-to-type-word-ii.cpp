cba40b41b03420b5cb6bbdcc7d54f6ac88c6154f
tarunsilam07 <tarun79767@gmail.com>
2024-08-06 11:08:49 +0530

    Time: 131 ms (19.75%) | Memory: 29.6 MB (5.55%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        unordered_map<char,int>mpp;
+        for(int i=0;i<word.size();i++){
+            mpp[word[i]]++;
+        }
+        vector<int>count;
+        for(auto it:mpp){
+            count.push_back(it.second);
+        }
+        sort(count.rbegin(),count.rend());
+        if(count.size()<=8) return word.size();
+        int push=0;
+        for(int i=0;i<count.size();i++){
+           double k=ceil((i+1)/8.0);
+           cout<<k;
+           push=push+count[i]*k;
+        }
+        return push;
+    }
+};
