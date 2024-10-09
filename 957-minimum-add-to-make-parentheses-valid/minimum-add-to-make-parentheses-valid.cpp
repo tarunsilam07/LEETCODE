1855ae54d812ed646072034d0962d5ebdfde16ef
tarunsilam07 <tarun79767@gmail.com>
2024-10-09 10:22:08 +0530

    Time: 3 ms (42.94%) | Memory: 8 MB (32.94%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
+        int n=s.size();
         stack<char>st;
         int cnt=0;
-        for(int i=0;i<s.size();i++)
-        {
+        for(int i=0;i<n;i++){
             char ch=s[i];
-            if(ch=='(' || ch=='{' || ch=='[')
+            if(ch=='(')
             st.push(ch);
-            else
-            {
-                if(!st.empty())
-                {
-                    char top=st.top();
-                    if((ch=='}' && top=='{') || (ch==']' && top=='[') || (ch==')' && top=='('))
-                    {
-                        st.pop();
-                    }
-                    else cnt++;
-                }
-                else if(st.empty()) cnt++;
+            else if(!st.empty()){
+                char top=st.top();
+                if(ch==')' && top=='(')
+                st.pop();
+                else cnt++;
             }
+            else cnt++;
         }
         return cnt+st.size();
     }
